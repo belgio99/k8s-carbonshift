b2556ae49cf3ff282ee2380400575926b8e93066
belgio99 <17623601+belgio99@users.noreply.github.com>
2025-05-02 08:22:28 +0200

    Add simple and static decision engine

---
+++ decision-engine/decision-engine.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_decision():
+    # Calculate the valid until time
+    valid_until = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=1)
+    valid_until_iso = valid_until.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    decision = {
+      "directWeight": 80,
+      "queueWeight": 20,
+      "flavorWeights": {
+        "low-power": 50,
+        "mid-power": 30,
+        "high-power": 0
+      },
+      "deadlines": {
+        "low-power": 600,
+        "mid-power": 120,
+        "high-power": 30
+      },
+      "validUntil": valid_until_iso
+    }
+    return jsonify(decision)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
